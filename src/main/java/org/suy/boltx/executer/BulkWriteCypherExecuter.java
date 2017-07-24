0f652323c389ce4e91f1859612fdb08e559d83c5
Masashi Umezawa <ume@softumeya.com>
2017-07-24 18:12:58 +0900

    Moved msg.reply() for fast response

--- src/main/java/org/suy/boltx/executer/BulkWriteCypherExecuter.java
+++ src/main/java/org/suy/boltx/executer/BulkWriteCypherExecuter.java
@@ -1,10 +1,14 @@
 package org.suy.boltx.executer;
 
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.exceptions.Neo4jException;
 
 import java.util.HashMap;
 
@@ -22,8 +26,29 @@ public class BulkWriteCypherExecuter extends BulkCypherExecuter {
     vertx.<JsonObject>executeBlocking(future -> {
       basicExecute(msg, future);
     }, false, res -> {
-      msg.reply(res.result());
+
     });
+  }
+
+  protected void basicExecute(Message<JsonObject> msg,  Future<JsonObject> future){
+    JsonObject bulkResult;
+    try (Session session = driver.session())
+    {
+      try (Transaction tx = session.beginTransaction())
+      {
+        JsonObject body = msg.body();
+        JsonArray statements = body.getJsonArray("statements");
+        statements.forEach(each -> {
+          this.processStatement((JsonObject) each, tx);
+        });
+        bulkResult = TrueValue;
+        msg.reply(bulkResult);
+      } catch (Neo4jException ex) {
+        bulkResult = FalseValue;
+        msg.reply(bulkResult);
+      }
+    }
+    future.complete(bulkResult);
   }
 
   protected void processStatement(JsonObject statementJson, Transaction transaction) {
